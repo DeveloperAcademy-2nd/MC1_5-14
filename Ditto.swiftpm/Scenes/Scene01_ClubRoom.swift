aff605e29a58e9a6b4de8180725d01b13a1dece0
kpk0616 <kpk0616@naver.com>
2023-03-29 22:13:04 +0900

    [Add] #1 - Scene01_ClubRoom 추가

---
+++ Ditto.swiftpm/Scenes/Scene01_ClubRoom.swift
@@ -0,0 +1,20 @@
+//
+//  Scene01_ClubRoom.swift
+//  
+//
+//  Created by 박의서 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct Scene01_ClubRoom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Scene01_ClubRoom_Previews: PreviewProvider {
+    static var previews: some View {
+      Scene01_ClubRoom()
+    }
+}
