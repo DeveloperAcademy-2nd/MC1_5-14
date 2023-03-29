25d00b30a03ddd4f0cb94dc0c491375ee69247dd
kpk0616 <kpk0616@naver.com>
2023-03-29 21:23:49 +0900

    [Add] #1 - Scene00_Intro 추가

---
+++ Ditto.swiftpm/Scenes/Scene00_Intro.swift
@@ -0,0 +1,20 @@
+//
+//  IntroScene00.swift
+//  
+//
+//  Created by 박의서 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct Scene00_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Scene00_Intro_Previews: PreviewProvider {
+    static var previews: some View {
+      Scene00_Intro()
+    }
+}
