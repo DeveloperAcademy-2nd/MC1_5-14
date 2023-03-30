dbd89164d4736d7c7fb048cb15a3b397fc16f1dd
seongjun Jang <peinguin77@gmail.com>
2023-03-31 05:16:16 +0900

    [Add] Jun's Code

---
+++ Ditto.swiftpm/Scenes/Scene02_HallWay.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Jun on 2023/03/31.
+//
+
+import SwiftUI
+
+struct Scene02_HallWay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Scene02_HallWay_Previews: PreviewProvider {
+    static var previews: some View {
+        Scene02_HallWay()
+    }
+}
--- Ditto.swiftpm/Views/SceneView.swift
+++ Ditto.swiftpm/Views/SceneView.swift
@@ -10,40 +10,30 @@ import SwiftUI
 struct SceneView: View {
     
     @EnvironmentObject var data: Data
-    @State private var isHiding = [false,true,true,true]
-    @State private var presentEpisode: Int = 0
+    @State var isHiding = [false, true, true, true, true, true, true]
     
-    let bgImages = ["Ditto", "ClubRoom"]
+    let bgImages = ["bgDitto", "bgClubRoom", "bgHallWay", "bgClassRoom", "bgSkyView", "bgNightOcean", "bgCherryBlossom", "bgEnding"]
     
     var body: some View {
         NavigationView {
             ZStack{
-                Image(bgImages[presentEpisode])
+                Image(bgImages[data.presentEpisode])
                   .resizable()
                   .edgesIgnoringSafeArea(.all)
                 VStack{
                     NavigationLink(destination:
-                                    Scene00_Intro()
-                        .environmentObject(Data())){
-                            Scene00_Content()
+                                    Scene00_Intro()){
+                        Scene00_Content(isHiding:$isHiding)
                                 .isHidden(isHiding[0])
-                                .simultaneousGesture(TapGesture().onEnded{
-                                    isHiding[0].toggle()
-                                    isHiding[1].toggle()
-                                    presentEpisode = 1
-                                })
                         }
                     NavigationLink(destination:
-                                    Scene01_ClubRoom()
-                        .environmentObject(Data())
-                        .navigationBarBackButtonHidden(true)){
-                        Scene01_Content()
-                            .isHidden(isHiding[1])
-                            .simultaneousGesture(TapGesture().onEnded{
-                                isHiding[1].toggle()
-                                isHiding[0].toggle()
-                                presentEpisode = 0
-                            })
+                                    Scene01_ClubRoom()){
+                        Scene01_Content(isHiding:$isHiding)
+                                .isHidden(isHiding[1])
+                    }
+                    NavigationLink(destination:
+                                    Scene02_HallWay()){
+                        Scene02_Content(isHiding:$isHiding)
                     }
                 }
             }
@@ -52,30 +42,42 @@ struct SceneView: View {
 }
 
 struct Scene00_Content: View {
+    @Binding var isHiding: [Bool]
+    @EnvironmentObject var data: Data
+    
+    init(isHiding: Binding<[Bool]>) {
+        self._isHiding = isHiding
+    }
+    
     var body: some View {
         VStack {
           ZStack {
-            Text("        ")
-              .padding(.horizontal, 34)
-              .padding(.vertical, 14)
-              .foregroundColor(.white)
-              .background(Color.init(hex: 0x8DADAB, alpha: 0.7))
-              .cornerRadius(90)
-              .frame(maxHeight: .infinity, alignment: .bottom)
             VStack {
-              Image("START")
+            Spacer()
+              Image("SCENE01")
                 .resizable()
                 .frame(width: 53, height: 19)
                 .frame(maxHeight: .infinity, alignment: .bottom)
-              Spacer().frame(height: 15)
+              Spacer()
             }
           }
-          Spacer().frame(height: 33)
+        }.onTapGesture {
+            isHiding[0].toggle()
+            isHiding[1].toggle()
+            data.presentEpisode = 1
         }
     }
 }
 
 struct Scene01_Content: View {
+    
+    @Binding var isHiding: [Bool]
+    @EnvironmentObject var data: Data
+    
+    init(isHiding: Binding<[Bool]>) {
+        self._isHiding = isHiding
+    }
+    
     var body: some View {
         VStack {
           ZStack {
@@ -95,6 +97,37 @@ struct Scene01_Content: View {
             }
           }
           Spacer().frame(height: 33)
+        }.onTapGesture {
+            isHiding[1].toggle()
+            isHiding[0].toggle()
+            isHiding[2].toggle()
+            data.presentEpisode = 2
+        }
+    }
+}
+    
+
+struct Scene02_Content: View {
+    
+    @Binding var isHiding: [Bool]
+    @EnvironmentObject var data: Data
+    
+    init(isHiding: Binding<[Bool]>) {
+        self._isHiding = isHiding
+    }
+    
+    var body: some View {
+        VStack {
+          Image("SCENE 02")
+            .resizable()
+            .frame(width: 53, height: 19)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+          Spacer().frame(height: 15)
+        }.onTapGesture {
+            isHiding[2].toggle()
+            isHiding[0].toggle()
+            isHiding[3].toggle()
+            data.presentEpisode = 3
         }
     }
 }
