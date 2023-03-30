8277ee1d65b45ae315d28f3e6cb4660c285dfd3c
seongjun Jang <peinguin77@gmail.com>
2023-03-31 05:41:20 +0900

    [Feat] Add showingBook in Data

--- Ditto.swiftpm/Scenes/Scene02_HallWay.swift
+++ Ditto.swiftpm/Scenes/Scene02_HallWay.swift
@@ -1,20 +1,38 @@
 //
-//  SwiftUIView.swift
+//  Scene02_Hallway.swift
 //  
 //
-//  Created by Jun on 2023/03/31.
+//  Created by 박의서 on 2023/03/31.
 //
 
 import SwiftUI
 
-struct Scene02_HallWay: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Scene02_Hallway: View {
+  @EnvironmentObject var data: Data
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Image("Background_ep1")
+          .resizable()
+          .ignoresSafeArea(.all)
+        VStack {
+          HStack {
+            Spacer().frame(width: 600)
+            Button(action: {
+            }, label: {
+              NavigationLink(destination: Scene01_ClubRoom_Main()
+                .environmentObject(data)
+                .navigationBarBackButtonHidden(true)) {
+                Image("Button_Hallway")
+                  .resizable()
+                  .frame(width: 36, height: 36)
+                  .frame(maxHeight: .infinity, alignment: .bottomTrailing)
+              }
+            })
+          }
+          Spacer().frame(height: 17)
+        }
+      }
     }
-}
-
-struct Scene02_HallWay_Previews: PreviewProvider {
-    static var previews: some View {
-        Scene02_HallWay()
-    }
+  }
 }
