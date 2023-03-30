ab8c7f20bc6c9880e429be1524445bd6eb07a5b7
seongjun Jang <peinguin77@gmail.com>
2023-03-30 22:30:31 +0900

    [Del] sheetManager Deleted

--- Ditto.swiftpm/Ditto.swift
+++ Ditto.swiftpm/Ditto.swift
@@ -2,13 +2,9 @@ import SwiftUI
 
 @main
 struct Ditto: App {
-    
-    @StateObject var sheetManager = SheetManager()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(sheetManager)
         }
     }
 }
