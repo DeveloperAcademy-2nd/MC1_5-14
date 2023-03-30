7ef2b1c1e37f91428834cd7a504eb6d4d54ef01f
kpk0616 <kpk0616@naver.com>
2023-03-30 16:43:04 +0900

    [Add] #1 - Scene08_ClubRoom_Ending 추가

---
+++ Ditto.swiftpm/Scenes/Scene08_ClubRoom_Ending.swift
@@ -0,0 +1,20 @@
+//
+//  Scene08_ClubRoom_Ending.swift
+//  
+//
+//  Created by 박의서 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct Scene08_ClubRoom_Ending: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Scene08_ClubRoom_Ending_Previews: PreviewProvider {
+    static var previews: some View {
+      Scene08_ClubRoom_Ending()
+    }
+}
